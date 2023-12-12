77700d7bd15d69093aa587cc30849d39529f15c9
dev-taherm <tahermahram0@gmail.com>
2023-12-12 08:48:54 -1000

    morph function in post model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Relation::morphMap([
+            'customer' => 'App\Models\Customer',
+            'seller' => 'App\Models\Seller',
+        ]);
     }
 
     /**
